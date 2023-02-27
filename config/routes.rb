570585c35e1ff54bf762cc5e738e6404e3547c6d
kamaljoshi7465 <kamaljoshi673243@gmail.com>
2023-02-27 11:24:16 +0530

    ActiveAdmin&API

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  resources :students
+  resources :accounts
+  resources :courses
+  resources :animals
+  namespace :api do 
+    namespace :v1 do 
+      resources :students 
+      resources :courses 
+      resources :animals
+      resources :accounts
+    end 
+  end
+end
